f543dd75a3a7e7032231d51d4c0a0bf6c1b8189f
Byiringiro <byives21@gmail.com>
2019-07-09 23:12:11 +0000

    update wholesale

--- resources/views/wholesale/tools.blade.php
+++ resources/views/wholesale/tools.blade.php
@@ -11,47 +11,40 @@
                     <p class="p1">Tools</p>
                     <p> Tell the users about most of your store</p>
                 </div>
-            <div class="row row2">
-                    <div class="divdash col-md-6">
-                        <h4>1. Products</h4>
-                        <p>Create products  in your store with attractive pricing, discounts, photos etc
-                        </p>
-                    </div>
-                    <div class="divdash col-md-6">
-                    <img src="{{asset('frontEnd/imagepackages/disc2.jpeg')}}" class="share img-responsive"  alt="" />
+            <div class="row4">
+                <div>
+                    <h4>1.Products</h4>
+                     <p>Create products  in your store with attractive pricing, discounts, photos etc
+                    </p>
+                </div>
+                <div>
+                <img src="{{asset('frontEnd/imagepackages/disc2.jpeg')}}" class="share img-responsive"  alt="" />
 
-                    </div>
                 </div>
-                <div class="row row2">
-                    <div class="divdash col-md-6">
-                    <img src="{{asset('frontEnd/imagepackages/Purple-african-women-shoes-and-bag-set-italian-newest-2018.jpg')}}" class="share img-responsive"  alt="" />
-                    </div>
-                    <div class="divdash col-md-6">
-                        <h4> 2. Photos </h4>
-                        <p>Upload attractive photos to attract more and more customers to your store.
-                        </p>
-                    </div>
+                <div>
+                <img src="{{asset('frontEnd/imagepackages/photo.jpeg')}}" class="share img-responsive"  alt="" />
+
                 </div>
-                <div class="row row2">
-                    <div class="divdash col-md-6">
-                        <h4>3. Offers</h4>
-                        <p>Offer great discounts on your store to attract more users and increase your sales.
-                        </p>
-                    </div>
-                    <div class="divdash col-md-6">
-                    <img src="{{url('products/small/')}}">
-                    </div>
+                <div>
+                    <h4> 2. Photos </h4>
+                    <p>Upload attractive photos to attract more and more customers to your store.</p>
                 </div>
-                <div class="row row2">
-                    <div class="divdash col-md-6">
-                    <img src="{{url('products/small/')}}">
-                    </div>
-                    <div class="divdash col-md-6">
-                        <h4> 4. Form </h4>
-                        <p>Gather feedback about your store to know what more your users want from your store,
-                            what problems the have, etc
-                        </p>
-                    </div>
+                <div>
+                    <h4>3. Offers</h4>
+                    <p>Offer great discounts on your store to attract more users and increase your sales.</p>
+                </div>
+                <div>
+                <img src="{{asset('frontEnd/imagepackages/discount20.jpeg')}}" class="share img-responsive"  alt="" />
+
+                </div>
+                <div>
+                <img src="{{asset('frontEnd/imagepackages/form.jpg')}}" class="share img-responsive"  alt="" />
+                </div>
+                <div>
+                    <h4> 4. Form </h4>
+                    <p>Gather feedback about your store to know what more your users want from your store,
+                     what problems the have, etc</p>
+                </div>
                 </div>
             </div><!-- container-fluid finish -->
     </div><!-- wrapper finish -->
